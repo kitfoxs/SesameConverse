335ce3734df60343bfe3ff96c335dc74e9dd57c7
jazir5 <31750063+jazir555@users.noreply.github.com>
2025-03-14 02:38:46 -0700

    Update SesameConverse.py

--- SesameConverse.py
+++ SesameConverse.py
@@ -14,6 +14,7 @@ import traceback
 import platform
 import signal
 from generator import load_csm_1b, Segment
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # Suppress unnecessary warnings
 warnings.filterwarnings("ignore", category=UserWarning)
